7cae9684dd89639d1012cb38c2c5abb26bd2dca6
Pratik Wani <wanipratik30@gmail.com>
2026-03-30 20:30:36 +0530

    connected backend

--- ai-fitness-richness/src/pages/Login.jsx
+++ ai-fitness-richness/src/pages/Login.jsx
@@ -11,10 +11,13 @@ function Login() {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
-        email,
-        password,
-      });
+      const res = await axios.post(
+        "https://ai-fitness-backend.onrender.com/api/auth/login",
+        {
+          email,
+          password,
+        },
+      );
 
       const token = res.data.token;
 
--- ai-fitness-richness/src/pages/Signup.jsx
+++ ai-fitness-richness/src/pages/Signup.jsx
@@ -9,10 +9,13 @@ function Signup() {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/signup", {
-        email,
-        password,
-      });
+      const res = await axios.post(
+        "https://ai-fitness-backend.onrender.com/api/auth/signup",
+        {
+          email,
+          password,
+        },
+      );
 
       alert(res.data.message);
 
